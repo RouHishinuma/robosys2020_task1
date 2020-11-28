eeafb537978f6ab19d00e11ea09b46389a03bb74
shimotoriharuki <underbird.h@gmail.com>
2020-11-28 02:28:35 +0000

    sushi done

--- myled.c
+++ myled.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/uaccess.h>
 MODULE_AUTHOR("Haruki Shimotori and Ryuchi Ueda");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
@@ -8,16 +10,35 @@ MODULE_VERSION("0.0.1");
 
 static dev_t dev;
 static struct cdev cdv;
+static struct class *cls = NULL;
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
-	printk(KERN_INFO "led_write is called\n");
+	char c;
+	if(copy_from_user(&c, buf, sizeof(char)))
+		return -EFAULT;
+
+	printk(KERN_INFO "receive %c\n", c);
 	return 1;
 }
 
+
+static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos)
+{
+	int size = 0;
+	char sushi[] = {'s','u', 's', 'h', 'i'};
+	if(copy_to_user(buf + size, (const char *)sushi, sizeof(sushi))){
+		printk(KERN_ERR "sushi : popy_to_user failed\n");
+		return -EFAULT;
+	}
+	size += sizeof(sushi);
+
+	return size;
+}
 static struct file_operations led_fops = {
 	.owner = THIS_MODULE,
-	.write = led_write
+	.write = led_write,
+	.read = sushi_read
 };
 
 static int __init init_mod(void) 
@@ -37,12 +58,21 @@ static int __init init_mod(void)
 		printk(KERN_ERR "cdev_add falid. major:%d, minor:%d\n", MAJOR(dev), MINOR(dev));
 		return retval;
 	}	
+
+	cls = class_create(THIS_MODULE, "myled");
+	if(IS_ERR(cls)){
+		printk(KERN_ERR "class_create failed");
+		return PTR_ERR(cls);
+	}
+	device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
 	return 0;
 }
 
 static void __exit cleanup_mod(void) 
 {
 	cdev_del(&cdv);
+	device_destroy(cls, dev);
+	class_destroy(cls);
 	unregister_chrdev_region(dev, 1);
 	printk(KERN_INFO "%s is unloaded. mojor:%d\n", __FILE__, MAJOR(dev));
 }
