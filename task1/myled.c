713b36fca2b19ad0fe4f32730460e6f73a685f29
shimotoriharuki <underbird.h@gmail.com>
2020-12-04 14:00:46 +0000

    fix

--- task1/myled.c
+++ task1/myled.c
@@ -127,14 +127,13 @@ void fan_on_off(void)
 	irled_write('e');
 
 }
+
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
 	char c;
 	if(copy_from_user(&c, buf, sizeof(char)))
 		return -EFAULT;
 
-	//printk(KERN_INFO "receive %c\n", c);
-	
 	if(c == '1'){
 		fan_on_off();
 	}
